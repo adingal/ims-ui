eb00b67c91b35b42b449b99222521b9af010fcb4
Alex Dingal <lhexie2014@gmail.com>
2024-01-29 14:27:56 +0800

    Add login icon and mobile logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,10 +19,6 @@ const HeaderLinks: NavLinks = [
     route: "/loadboard",
     text: "LoadBoard",
   },
-  {
-    route: "/login",
-    text: "Login",
-  },
 ];
 
 function Header() {
@@ -46,10 +42,17 @@ function Header() {
           <div>
             <Link href="/">
               <Image
-                className="object-cover"
+                className="hidden lg:inline-block object-cover"
                 src="/ims-logo.png"
                 height={90}
                 width={48}
+                alt="Inventory Management System"
+              />
+              <Image
+                className="lg:hidden object-cover"
+                src="/ims-logo-sm.png"
+                height={58}
+                width={58}
                 alt="Inventory Management System"
               />
             </Link>
@@ -57,6 +60,28 @@ function Header() {
           <nav>
             <ul className="flex flex-row items-center justify-end gap-2 md:gap-3 lg:gap-4 font-sans text-xs md:text-sm lg:text-base uppercase tracking-wider font-normal text-white">
               {renderHeaderLinks}
+              <li>
+                <Link
+                  href="/login"
+                  className="text-white transition-colors hover:text-gray-400 cursor-pointer"
+                  title="Login"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 md:w-7 md:h-7"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                </Link>
+              </li>
             </ul>
           </nav>
         </div>
