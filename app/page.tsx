5e978d750055b6820d9fa0ecd54f4bf420e16708
Alex Dingal <lhexie2014@gmail.com>
2024-01-29 22:40:40 +0800

    Add image assets and integrate table component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import Header from '@/components/Header'
+import React from "react";
+import Header from "@/components/Header";
+import LoadBoardTable from "@/components/LoadBoardTable";
+import Container from "@/atoms/Container";
+import PageTitle from "@/atoms/PageTitle";
+
+type LoadBoardProps = {
+  serialNo: string;
+  status: string;
+  location: string;
+  type: string;
+  addedBy: string;
+  addedDate: Date;
+};
 
 function Home() {
   return (
     <>
       <Header />
-      <main>
-        <h1>Home</h1>
+      <main className="py-6 md:py-8 lg:py-10">
+        <Container>
+          <PageTitle title="LoadBoard" imgSource="/loadboard-icon.svg" />
+          <LoadBoardTable />
+        </Container>
       </main>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
